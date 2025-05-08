35b21f3a25991936e0dae3592afbfa7044ae8901
MAHA SRIEE PRASENNA B <mahasrieeprasenna.22ads@sonatech.ac.in>
2025-05-08 17:29:01 +0530

    Add files via upload

--- doctor/frontend/App.js
+++ doctor/frontend/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DoctorsList from './components/DoctorsList';
 import Header from './components/Header';
-import './styles/App.css';
+import './App.css';
 
 function App() {
   return (
